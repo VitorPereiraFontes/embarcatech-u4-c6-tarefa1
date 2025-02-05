f0f930c084e7e4d71419aaba7408b6bcbb139f20
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-02-05 17:28:04 -0300

    Configurando as interrupções para os botões
    Implementando a função de callback que será disparada pela interrupção
    gerada pelos botões A e B.
    
    Inicializando e configurando a interrupção para as GPIO's conectadas aos
    botões A e B.

--- src/main.c
+++ src/main.c
@@ -18,11 +18,53 @@
 #define LED_RGB_MASK 1 << RED_LED_PIN | 1 << GREEN_LED_PIN | 1 << BLUE_LED_PIN // Mascara de bits para inicializar o LED RGB
 #define LED_RGB_GPIO_DIR_MASK 1 << RED_LED_PIN | 1 << GREEN_LED_PIN | 1 << BLUE_LED_PIN // Mascara de bits para configurar o LED RGB como saída
 
+// Função que será disparada quando a interrupção for atendida
+void buttons_irq_handler(uint gpio, uint32_t events){
+    // Obtém o tempo atual em microssegundos
+    uint64_t current_time = to_us_since_boot(get_absolute_time());
+
+    // Cria uma variável para armazenar o tempo, microssegundos, que o último evento aconteceu
+    static volatile uint64_t last_event_time = 0;
+
+    // Verifica se já se passaram 300 ms desde o último evento de acionamento do botão, prevenindo o debouncing
+    if (current_time - last_event_time > DEBOUNCING_INTERVAL){
+        last_event_time = current_time;
+
+        // Verifica qual botão foi pressionado para controlar o LED RGB
+        switch (gpio){
+            case BUTTON_A_PIN:
+                static volatile bool green_led_on = false; // Variável que controla o estado do LED verde
+
+                printf("Botão A pressionado! Alterando o estado do LED verde...\n");
+
+                gpio_put(GREEN_LED_PIN,!green_led_on); // Acendendo/Desligando o LED verde
+
+                green_led_on = !green_led_on; // Atualizando o estado da variável de controle
+            break;
+
+            case BUTTON_B_PIN:
+                static volatile bool blue_led_on = false; // Variável que controla o estado do LED verde
+
+                printf("Botão B pressionado! Alterando o estado do LED verde...\n");
+
+                gpio_put(BLUE_LED_PIN,!blue_led_on); // Acendendo/Desligando o LED verde
+
+                blue_led_on = !blue_led_on; // Atualizando o estado da variável de controle
+            break;
+
+            default:
+            break;
+        }
+    }
+}
+
 int main(){
     stdio_init_all(); // Inicializa a comunicação serial via USB
 
     gpio_init_mask(LED_RGB_MASK); // Inicializa o LED RGB
     gpio_set_dir_masked(LED_RGB_MASK,LED_RGB_GPIO_DIR_MASK); // Coloca todos os pinos do LED RGB como saída em nível baixo
+
+    initialize_buttons(BUTTON_A_PIN,BUTTON_B_PIN,buttons_irq_handler); // Inicializa e configura a interrupção para os botões A e B
 
     while (true) {
         printf("Hello, world!\n");
